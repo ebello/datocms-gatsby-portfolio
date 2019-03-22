1b3818f0486097e730f77cf95b3e284a3ba4e2e9
Matteo Giaccone <matteo@turnintocoders.it>
2019-03-22 16:20:50 +0100

    Fixed missing sidebar in about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,24 +2,27 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import { HelmetDatoCms } from 'gatsby-source-datocms'
 import Img from 'gatsby-image'
+import Layout from "../components/layout"
 
 const About = ({ data: { about } }) => (
-  <article className="sheet">
-    <HelmetDatoCms seo={about.seoMetaTags} />
-    <div className="sheet__inner">
-      <h1 className="sheet__title">{about.title}</h1>
-      <p className="sheet__lead">{about.subtitle}</p>
-      <div className="sheet__gallery">
-        <Img fluid={about.photo.fluid} />
+  <Layout>
+    <article className="sheet">
+      <HelmetDatoCms seo={about.seoMetaTags} />
+      <div className="sheet__inner">
+        <h1 className="sheet__title">{about.title}</h1>
+        <p className="sheet__lead">{about.subtitle}</p>
+        <div className="sheet__gallery">
+          <Img fluid={about.photo.fluid} />
+        </div>
+        <div
+          className="sheet__body"
+          dangerouslySetInnerHTML={{
+            __html: about.bioNode.childMarkdownRemark.html,
+          }}
+        />
       </div>
-      <div
-        className="sheet__body"
-        dangerouslySetInnerHTML={{
-          __html: about.bioNode.childMarkdownRemark.html,
-        }}
-      />
-    </div>
-  </article>
+    </article>
+  </Layout>
 )
 
 export default About
